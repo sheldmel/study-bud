f3ee540dee10622cf826c7b1f9ca44c1631ebb81
unknown <dmello.shelton@yahoo.com>
2021-11-23 09:56:52 -0500

    added Topic, Room and Message models

--- base/views.py
+++ base/views.py
@@ -1,21 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
-
-rooms = [
-    {'id':1, 'name': 'Lets learn python'},
-    {'id':2, 'name': 'Design with me'},
-    {'id':3, 'name': 'Frontend dev'},
-]
+from .models import Room
 
 def home(request):
+    rooms = Room.objects.all()
     data = {'rooms':rooms}
     return render(request, 'base/home.html', data)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id = pk)
     data = {'room':room}
     return render(request, 'base/room.html', data)
