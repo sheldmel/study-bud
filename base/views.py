2900e81cdc77b872634dcd9c34bfb357ff464892
unknown <dmello.shelton@yahoo.com>
2021-11-25 12:39:55 -0500

    completed user prfile page, activity feed component and chat room messages functionality

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 def loginPage(request):
@@ -53,6 +53,7 @@ def registerUser(request):
             return redirect('home')
         else:
             messages.error(request, 'An error occured. Please try again')
+            
     data = {'page':page, 'form':form}
     return render(request,'base/login_register.html', data)
 
@@ -67,14 +68,33 @@ def home(request):
         Q(description__icontains=q)
     )
     room_count = rooms.count()
-    data = {'rooms':rooms, 'topics':topic, 'room_count':room_count}
+    activity_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    data = {'rooms':rooms, 'topics':topic, 'room_count':room_count, 'activity_messages': activity_messages}
 
     return render(request, 'base/home.html', data)
 
 def room(request, pk):
     room = Room.objects.get(id = pk)
-    data = {'room':room}
+    room_messages = room.message_set.all().order_by('created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    data = {'room':room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', data)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    activity_messages = user.message_set.all()
+    topic = Topic.objects.all()
+    data = {'user': user, 'rooms': rooms, 'activity_messages': activity_messages, 'topics': topic}
+    return render(request, 'base/profile.html', data)
 
 @login_required(login_url='/login')
 def createRoom(request):
@@ -82,7 +102,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host - request.user
+            room.save
             return redirect('home')
 
     data = {'form': form}
@@ -115,3 +137,15 @@ def deleteRoom(request, pk):
         return redirect('home')
         
     return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='/login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('Your are not allowed to delete this message!')
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+        
+    return render(request, 'base/delete.html', {'obj': message})
